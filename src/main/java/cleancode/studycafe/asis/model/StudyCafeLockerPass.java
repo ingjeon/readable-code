cba857470755bb8130084e7bc734e70cda66faa0
ingjeon <minjae77777@gmail.com>
2024-10-09 22:42:56 +0900

    [6-2] 객체의 책임과 응집도

--- src/main/java/cleancode/studycafe/asis/model/StudyCafeLockerPass.java
+++ src/main/java/cleancode/studycafe/asis/model/StudyCafeLockerPass.java
@@ -15,7 +15,12 @@ public class StudyCafeLockerPass {
     public static StudyCafeLockerPass of(StudyCafePassType passType, int duration, int price) {
         return new StudyCafeLockerPass(passType, duration, price);
     }
-
+    public boolean isSamePassType(StudyCafePassType passType) {
+        return this.passType == passType;
+    }
+    public boolean isSameDuration(int duration) {
+        return this.duration == duration;
+    }
     public StudyCafePassType getPassType() {
         return passType;
     }
