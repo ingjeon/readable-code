8c94587776571455d000624c040a6a98deda9184
ingjeon <minjae77777@gmail.com>
2024-10-09 22:38:38 +0900

    test

--- src/main/java/cleancode/studycafe/asis/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/asis/StudyCafeApplication.java
@@ -5,6 +5,7 @@ public class StudyCafeApplication {
     public static void main(String[] args) {
         StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
         studyCafePassMachine.run();
+        //test
     }
 
 }
